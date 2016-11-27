adf84fc5852b10351f37eb43cb86e61be62a9775
Steven Marr <StevenMarr@me.com>
2016-11-27 14:21:24 -0700

    fix type in INSTALLED_APPS

--- presentation_manager/settings.py
+++ presentation_manager/settings.py
@@ -34,7 +34,7 @@ AUTH_USER_MODEL = 'users.User'
 # Application definition
 
 INSTALLED_APPS = [
-    'users.apps.UsersConfig'
+    'users.apps.UsersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
