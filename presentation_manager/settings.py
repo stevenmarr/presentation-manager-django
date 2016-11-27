6a9c79cf0782a5694b9264b9a2baec064593b314
Steven Marr <StevenMarr@me.com>
2016-11-27 14:23:05 -0700

    add presentations app

--- presentation_manager/settings.py
+++ presentation_manager/settings.py
@@ -35,6 +35,7 @@ AUTH_USER_MODEL = 'users.User'
 
 INSTALLED_APPS = [
     'users.apps.UsersConfig',
+    'presentations.apps.PresentationsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
