1711ed7acd8b2cf700f4b9ca4909464533c79efb
Steven Marr <StevenMarr@me.com>
2016-11-27 20:07:16 -0700

    add presentationadmin site

--- presentations/admin.py
+++ presentations/admin.py
@@ -1,5 +1,13 @@
 from django.contrib import admin
+from django.contrib.admin import AdminSite
 
 from .models import Presentation
+
+class PresentationAdminSite(AdminSite):
+    site_header = 'Presentation Manager'
+
+admin_site = PresentationAdminSite(name='presentationadmin')
+
+admin_site.register(Presentation)
 
 admin.site.register(Presentation)
