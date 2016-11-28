2f097d8b72b438aff33b08407009f470002ac0eb
Steven Marr <StevenMarr@me.com>
2016-11-27 19:03:29 -0700

    install django JET

--- presentation_manager/settings.py
+++ presentation_manager/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'conferences.apps.ConferencesConfig',
     'users.apps.UsersConfig',
     'presentations.apps.PresentationsConfig',
+    'jet',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
