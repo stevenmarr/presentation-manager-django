102980d050196227bdb5fe01ced0e0c4503ea8cf
Steven Marr <StevenMarr@me.com>
2016-11-27 22:27:59 -0700

    add index view

--- presentations/views.py
+++ presentations/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views import generic
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+from .models import Presentation
+
+
+@login_required
+def index(request, conference_id):
+	presentations = Presentation.objects.filter(conference_id=conference_id)
+	context = {'presentations': presentations, 'conference_id': conference_id}
+	return render(request, 'presentations/index.html', context)
