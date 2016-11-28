1f48c9dc1c862549cfcde5bf2faf4f20af315d3f
Steven Marr <StevenMarr@me.com>
2016-11-28 10:31:20 -0700

    add publish to dropbox boolean field

--- conferences/models.py
+++ conferences/models.py
@@ -7,3 +7,5 @@ class Conference(models.Model):
 	conference_text = models.CharField(max_length = 40)
 	start_date = models.DateField('Conference Start Date')
 	end_date = models.DateField('Conference End Date')
+	publish_presentations_to_dropbox = models.BooleanField(default = False)
+	
