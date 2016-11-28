d11d32efa21736668f8bb06a156a0c02ac759cc6
Steven Marr <StevenMarr@me.com>
2016-11-27 20:05:10 -0700

    remove time from Conference dates

--- conferences/models.py
+++ conferences/models.py
@@ -5,5 +5,5 @@ class Conference(models.Model):
 	def __str__(self):
 		return self.conference_text
 	conference_text = models.CharField(max_length = 40)
-	start_date = models.DateTimeField('Conference Start Date')
-	end_date = models.DateTimeField('Conference End Date')
+	start_date = models.DateField('Conference Start Date')
+	end_date = models.DateField('Conference End Date')
